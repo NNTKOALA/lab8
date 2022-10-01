3b3e96bed314648772faf9ecd62f6af4138d1b1a
NNTKOALA <trungnngch200639@fpt.edu.vn>
2022-10-01 21:40:16 +0700

    lab8

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const path = require('path')
 // database connect/access
 const mongoose = require('mongoose')
 const studentModel = require("./models/StudentSchema");
-const url = "mongodb://localhost:27017/greenwich"
-//const url = "mongodb+srv://nguyentrung:nntrung382k2@cluster.8b5c38m.mongodb.net/greenwich"
+//const url = "mongodb://localhost:27017/greenwich"
+const url = "mongodb+srv://nguyentrung:nntrung382k2@cluster.8b5c38m.mongodb.net/greenwich"
 mongoose.connect(url, {useNewUrlParser: true}, (err) => {
     if (err) {
         console.log(err)
