23931aaca064eafdf24f42156c2cb89b3eb5284f
NNTKOALA <trungnngch200639@fpt.edu.vn>
2022-10-01 17:21:44 +0700

    lab8

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
+//module import
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const path = require('path')
 
+// database connect/access
 const mongoose = require('mongoose')
 const studentModel = require("./models/StudentSchema");
-//const url = "mongodb://localhost:27017/greenwich"
-const url = "mongodb+srv://nguyentrung:nntrung382k2@cluster.8b5c38m.mongodb.net/greenwich"
+const url = "mongodb://localhost:27017/greenwich"
+//const url = "mongodb+srv://nguyentrung:nntrung382k2@cluster.8b5c38m.mongodb.net/greenwich"
 mongoose.connect(url, {useNewUrlParser: true}, (err) => {
     if (err) {
         console.log(err)
     }else{
         console.log("connect to db succeed !")
     }
+})
+
+//body-parser: get input from form
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extend : true}))
+
+//render ra form add
+app.get("/add", (req, res) => {
+  res.render("add") 
+})
+
+//nhận và xử lý dữ liệu từ form add
+app.post("/add", (req, res) => {
+  //console.log(req.body);
+  //res.send(req.body);
+  //res.render("output", { student: req.body });
+
+  var student = new studentModel({
+    name: req.body.name, 
+    age: req.body.age,
+    email: req.body.email,
+    image: req.body.image
+  })
+
+  student.save((err) =>{
+    if(err){
+      console.error(err)
+    }else{
+      res.redirect("student")
+    }
+  })
 })
 
 app.set('views', path.join(__dirname, 'views'))
@@ -26,6 +59,21 @@ app.get("/about", function(req, res) {
     res.render('about')
 })
 
+app.get("/login", (req, res) => {
+  res.render('login')
+})
+
+app.post("/login", (req, res) => {
+  var username = req.body.username;
+  var password = req.body.password;
+  var check = "Login failed !";
+  if (username == "admin" && password == "123456") {
+    check = "Login succeed !";
+  }
+  //res.send(check);
+  res.render("check", { result: check });
+});
+
 app.get("/student", (req, res) => {
     //select * from student
     studentModel.find((err, data) => {
@@ -37,7 +85,7 @@ app.get("/student", (req, res) => {
         //2. show dữ liệu ra API bằng "send"
         //res.send(data);
         //3. show dữ liệu ra view bằng "render"
-        res.render("student", {students : data});
+        res.render("student", {students : data})
       }
     })
 })
